b065eb460a8b5315fcad5dd8862f06997c82159c
Leonardo-Borges-AF <leoborgesaf@hotmail.com>
2025-07-22 19:22:02 -0300

    feat confronto Random weapon and turbo

--- src/index.js
+++ src/index.js
@@ -45,6 +45,31 @@ async function rollDice() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
+async function getRandomWeapon() {
+  let RandomWeapon = Math.random();
+  let result;
+
+  if (RandomWeapon < 0.5) {
+    result = "casco";
+  }
+  if (RandomWeapon > 0.5) {
+    result = "bomba";
+  }
+  return result;
+}
+async function getRandomTurbo() {
+  let RandomTurbo = Math.random();
+  let result;
+
+  if (RandomTurbo < 0.5) {
+    result = true;
+  }
+  if (RandomTurbo > 0.5) {
+    result = false;
+  }
+  return result;
+}
+
 async function getRandomBlock() {
   let randomBlock = Math.random();
   let result;
@@ -117,8 +142,14 @@ async function playRaceEngine(character1, character2) {
       );
     }
     if (block === "confronto") {
+      let weapon = await getRandomWeapon();
+
       let powerResult1 = character1.poder + diceResult1;
       let powerResult2 = character2.poder + diceResult2;
+
+      let losePoint = weapon === "casco" ? 1 : 2;
+
+      let turbo = await getRandomTurbo();
 
       console.log(`${character1.nome} confrontou ${character2.nome}! 🥊`);
 
@@ -127,20 +158,36 @@ async function playRaceEngine(character1, character2) {
 
       if (powerResult1 < powerResult2) {
         if (character1.pontos > 0) {
-          console.log(`${character2.nome} venceu o confronto!`);
-          character1.pontos--;
-          console.log(`${character1.nome} perdeu 1 ponto `);
+          console.log(
+            `${character2.nome} lançou um(a) ${weapon} venceu o confronto!`
+          );
+          character1.pontos - losePoint;
+          console.log(`${character1.nome} perdeu ${losePoint} ponto `);
         } else {
           console.log(`${character1.nome} não tem pontos para perder!`);
         }
+        character2.pontos = turbo ? character2.pontos + 1 : character2.pontos;
+        character2.pontos = turbo
+          ? console.log(`${character2.nome} ganhou um turbo (+ 1 ponto)`)
+          : console.log();
       } else if (powerResult2 < powerResult1) {
         if (character2.pontos > 0) {
-          console.log(`${character1.nome} venceu o confronto!`);
-          character2.pontos--;
-          console.log(`${character2.nome} perdeu 1 ponto `);
+          console.log(
+            `${character1.nome} lançou um(a) ${weapon} venceu o confronto!`
+          );
+          character2.pontos - losePoint;
+          console.log(`${character2.nome} perdeu ${losePoint} ponto `);
         } else {
           console.log(`${character2.nome} não tem pontos para perder!`);
         }
+        if (turbo) {
+          console.log(`${character2.nome} usou turbo e ganhou 1 ponto extra!`);
+          character2.pontos++;
+        }
+        character1.pontos = turbo ? character1.pontos + 1 : character1.pontos;
+        character1.pontos = turbo
+          ? console.log(`${character1.nome} ganhou um turbo (+ 1 ponto)`)
+          : console.log();
       } else {
         console.log("Empate no confronto! Ninguém perdeu ponto!");
       }
@@ -194,34 +241,39 @@ async function obterEntradaUsuario(pergunta) {
   );
 }
 
+// (async function main() {
+//   listarPersonagens();
+
+//   let personagem1, personagem2;
+
+//   while (!personagem1) {
+//     const entrada1 = await obterEntradaUsuario(
+//       "Digite o nome do primeiro personagem: "
+//     );
+//     personagem1 = encontrarPersonagem(entrada1);
+//     if (!personagem1) console.log("❌ Personagem inválido. Tente novamente.");
+//   }
+
+//   while (!personagem2 || personagem2 === personagem1) {
+//     const entrada2 = await obterEntradaUsuario(
+//       "Digite o nome do segundo personagem: "
+//     );
+//     personagem2 = encontrarPersonagem(entrada2);
+//     if (!personagem2) {
+//       console.log("❌ Personagem inválido. Tente novamente.");
+//     } else if (personagem2 === personagem1) {
+//       console.log("❌ Os personagens devem ser diferentes!");
+//       personagem2 = null;
+//     }
+//   }
+
+//   console.log(
+//     `🏁🚨 Corrida entre ${personagem1.nome} e ${personagem2.nome} começando!`
+//   );
+//   await playRaceEngine(personagem1, personagem2);
+// })();
+
 (async function main() {
-  listarPersonagens();
-
-  let personagem1, personagem2;
-
-  while (!personagem1) {
-    const entrada1 = await obterEntradaUsuario(
-      "Digite o nome do primeiro personagem: "
-    );
-    personagem1 = encontrarPersonagem(entrada1);
-    if (!personagem1) console.log("❌ Personagem inválido. Tente novamente.");
-  }
-
-  while (!personagem2 || personagem2 === personagem1) {
-    const entrada2 = await obterEntradaUsuario(
-      "Digite o nome do segundo personagem: "
-    );
-    personagem2 = encontrarPersonagem(entrada2);
-    if (!personagem2) {
-      console.log("❌ Personagem inválido. Tente novamente.");
-    } else if (personagem2 === personagem1) {
-      console.log("❌ Os personagens devem ser diferentes!");
-      personagem2 = null;
-    }
-  }
-
-  console.log(
-    `🏁🚨 Corrida entre ${personagem1.nome} e ${personagem2.nome} começando!`
-  );
-  await playRaceEngine(personagem1, personagem2);
+  console.log(`🏁🚨 entre ${Mario.nome} e ${Luigi.nome} começando`);
+  await playRaceEngine(Mario, Luigi);
 })();
